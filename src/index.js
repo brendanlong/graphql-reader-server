302debca8241fbecb4ca47ab84f0cd90915c9404
Brendan Long <self@brendanlong.com>
2018-02-24 12:28:07 -0500

    Setup prettier, better Babel, and the graphQL example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 // @flow
-const output: string = "hello";
-console.log(output);
+import { graphql, buildSchema } from "graphql";
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => "Hello world!"
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, "{ hello }", root).then(response => {
+  console.log(response);
+});
