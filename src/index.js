b456b76f52ceec4e2cde05d66b9419208f96efcd
Brendan Long <self@brendanlong.com>
2018-02-25 10:58:44 -0500

    Switch to the HTTP server version

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 // @flow
-import { graphql, buildSchema } from "graphql";
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import { buildSchema } from "graphql";
 
-// Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
-// The root provides a resolver function for each API endpoint
 const root = {
   hello: () => "Hello world!"
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, "{ hello }", root).then(response => {
-  console.log(response);
-});
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+app.listen(4000);
+console.log("Running GraphQL API server at localhost:4000/graphql");
