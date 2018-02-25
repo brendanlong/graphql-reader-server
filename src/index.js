2bc3e0828a3fa4f7915521d0b8e09027cf4e01d8
Brendan Long <self@brendanlong.com>
2018-02-25 12:48:57 -0500

    Add basic API skeleton

--- src/index.js
+++ src/index.js
@@ -1,16 +1,75 @@
 // @flow
 import express from "express";
 import graphqlHTTP from "express-graphql";
-import { buildSchema } from "graphql";
+import { makeExecutableSchema } from "graphql-tools";
+import { GraphQLDateTime } from "graphql-iso-date";
+import { GraphQLUrl } from "graphql-url";
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
+class Entry {
+  id: number;
+  feed: Feed;
+}
+
+class Feed {
+  id: number;
+  uri: string;
+  entries: Entry[];
+
+  constructor(id: number, { uri }: { uri: string }) {
+    this.id = id;
+    this.uri = uri;
+    this.entries = [];
   }
-`);
+}
+
+const schemaString = `
+  scalar DateTime
+  scalar URI
+
+  type Entry {
+    id: ID!
+    uri: URI
+    title: String
+    author: String
+    content: String
+    updated: DateTime
+    published: DateTime
+    feed: Feed!
+  }
+
+  type Feed {
+    id: ID!
+    uri: URI!
+    title: String
+    nickname: String
+    updated: DateTime
+    entries: [Entry!]!
+  }
+
+  type Query {
+    entries(first: Int, last: Int, publishedSince: DateTime, updatedSince: DateTime): [Entry!]!
+    feeds(first: Int, last: Int): [Feed!]!
+  }
+
+  type Mutation {
+    subscribeToFeed(uri: URI!): Feed
+  }
+`;
+
+const resolveFunctions = {
+  DateTime: GraphQLDateTime,
+  URI: GraphQLUrl
+};
+
+const schema = makeExecutableSchema({
+  typeDefs: schemaString,
+  resolvers: resolveFunctions
+});
 
 const root = {
-  hello: () => "Hello world!"
+  entries: () => [],
+  feeds: () => [],
+  subscribeToFeed: ({ uri }) => new Feed(1, { uri })
 };
 
 const app = express();
